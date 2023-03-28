1f0de34b17bb24df63feb57a87133885a84f9477
olamideabiodun <olamidesokunbi15@gmail.com>
2023-03-28 12:42:10 -0700

    _printf.c created by Faruq

---
+++ _printf.c
@@ -0,0 +1,44 @@
+#include "main.h"
+
+/**
+ * _printf - produces output according to a format
+ * @format: character string containing zero or more directives
+ *
+ * Return: number of characters printed (excluding null byte)
+ */
+int _printf(const char *format, ...)
+{
+	va_list args;
+	int i = 0, count = 0;
+
+	va_start(args, format);
+
+	while (format && format[i])
+	{
+		if (format[i] == '%')
+		{
+			i++;
+
+			if (!format[i])
+				return (-1);
+
+			if (format[i] == '%')
+				count += _putchar('%');
+			else if (format[i] == 'c')
+				count += _putchar(va_arg(args, int));
+			else if (format[i] == 's')
+				count += _puts(va_arg(args, char *));
+			else
+				count += _putchar('%') + _putchar(format[i]);
+		}
+		else
+			count += _putchar(format[i]);
+
+		i++;
+	}
+
+	va_end(args);
+
+	return (count);
+}
+
